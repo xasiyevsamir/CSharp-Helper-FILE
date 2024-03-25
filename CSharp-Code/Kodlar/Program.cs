115dfdb409d7b800c10a143f20604c62b3eefc11
xasiyevsamir <samirakh@code.edu.az>
2024-03-25 20:12:59 +0400

    update

--- CSharp-Code/Kodlar/Program.cs
+++ CSharp-Code/Kodlar/Program.cs
@@ -6,5 +6,6 @@ class Program
     {
         Console.WriteLine("Hello, World!");
         Console.WriteLine(args[0]);
+        Console.WriteLine(args[1]);
     }
 }
---
+++ CSharp-Code/Kodlar/Yorum_Sətri_Region.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace CSharp_Code.Kodlar
+{
+    internal class Yorum_Sətri_Region
+    {
+
+        public void RegionMethod()
+        {
+
+            // İki xətt ilə yorum sətri yaratmaq olur.Yalnız bir sətri yorum sətri edəcək.
+
+            /*
+             Bu simvollar ilədə bir neçə sətri yorum sətri etmək olur.
+
+            */
+
+
+            #region Kodlarim
+
+            Console.WriteLine("Kodlar");
+            #endregion
+
+        }
+
+
+
+
+    }
+}
